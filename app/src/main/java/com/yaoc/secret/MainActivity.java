61dfef5e5151b48ce753ff71ef41affcb95e4599
yaochen1224 <yao.chen1224@foxmail.com>
2017-03-08 20:25:09 -0500

    Added Firebase

--- app/src/main/java/com/yaoc/secret/MainActivity.java
+++ app/src/main/java/com/yaoc/secret/MainActivity.java
@@ -2,6 +2,11 @@ package com.yaoc.secret;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void save(View view) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("secret");
+
+        EditText editText = (EditText)findViewById(R.id.editTest);
+        String userSecret = editText.getText().toString();
+        myRef.setValue(userSecret);
+    }
+
+    public void reveal(View view) {
     }
 }
